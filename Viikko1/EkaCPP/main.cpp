42e356124378710daf7ed340f02016433b2f2003
Juhoro <juhoromppainen03@gmail.com>
2026-01-09 19:52:41 +0200

    Eka commit, testataan gitignoren toiminta

---
+++ Viikko1/EkaCPP/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int koe = 5;
+    printf("Hello WOrld C:n tavalla = %d\n", koe);
+    cout << "Hello World!" << koe << endl;
+    return 0;
+}
